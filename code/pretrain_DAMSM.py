98d1e8615e62a494cdb97bfc8b1f321321891af6
zaidalyafeai <alyafey22@gmail.com>
2021-03-03 03:50:31 +0300

    debug

--- code/pretrain_DAMSM.py
+++ code/pretrain_DAMSM.py
@@ -224,8 +224,8 @@ if __name__ == "__main__":
     ##########################################################################
     now = datetime.datetime.now(dateutil.tz.tzlocal())
     timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
-    output_dir = '../output/%s_%s_%s' % \
-        (cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
+    output_dir = '%s/%s_%s_%s' % \
+        (cfg.OUTPUT,cfg.DATASET_NAME, cfg.CONFIG_NAME, timestamp)
 
     model_dir = os.path.join(output_dir, 'Model')
     image_dir = os.path.join(output_dir, 'Image')
